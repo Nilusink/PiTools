bacc8165484fe6e5fb10ca7ff758ef26b2e6820c
Nilusink <nilusink@protonmail.com>
2024-05-24 19:40:28 +0200

    simplified the program + fixed it

--- ssh_tunnel_reconnector/main.py
+++ ssh_tunnel_reconnector/main.py
@@ -8,22 +8,18 @@ Author:
 Nilusink
 """
 from time import sleep, strftime
-from threading import Thread
 from subprocess import Popen
 from urllib import request
 from datetime import time
 import logging
-import socket
 import signal
 import sys
 
 
 SERVICE_NAME: str = "ssh_tunnels"
-PUBLIC_HOST: str = "server.nilus.ink"
-PUBLIC_I_HOST: str = "http://server.nilus.ink:80"
+PRIVATE_HOST: str = "http://server.nilus.ink:20080"
+PUBLIC_HOST: str = "http://server.nilus.ink:80"
 PING_INTERVAL: time = time(minute=2)
-ON_FAIL_DELAY: time = time(second=20)
-PORT: int = 20000
 
 
 def time_to_seconds(t: time) -> int:
@@ -36,7 +32,7 @@ def time_to_seconds(t: time) -> int:
     return t.second + t.minute * 60 + t.hour * 3600
 
 
-def check_connection(server: str = PUBLIC_I_HOST, timeout: float = 5) -> bool:
+def check_connection(server: str, timeout: float = 5) -> bool:
     """
     tries to request a GET from the given server
 
@@ -55,73 +51,23 @@ class PingPong:
     running: bool
 
     def __init__(self) -> None:
-        # create server socket
-        self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._server_socket.bind(("0.0.0.0", PORT))
-        self._server_socket.setblocking(False)
-        self._server_socket.settimeout(time_to_seconds(ON_FAIL_DELAY))
-        self._server_socket.setsockopt(
-            socket.SOL_SOCKET,
-            socket.SO_REUSEADDR,
-            1
-        )
-        self._server_socket.listen()
-
         self.running = True
-
-    def accept_ping(self) -> None:
-        """
-        waits for clients (itself) to connect and sends an 8-byte long message
-        """
-        while self.running:
-            # start client
-            t = Thread(target=self.ping)
-            t.start()
-
-            # using timeouts because otherwise the sock_accept function wouldn't
-            # notice self.running being set to false
-            try:
-                client, _ = self._server_socket.accept()
-
-            except TimeoutError:
-                sleep(time_to_seconds(ON_FAIL_DELAY))
-                continue
-
-            try:
-                client.sendall(b"-hellow-")
-
-            except (BrokenPipeError, ConnectionResetError) as e:
-                logging.warning(f"{strftime('%c')}: failed to SEND: \"{e}\"")
-
-            finally:
-                client.close()
-
-            # wait for client thread to finish
-            t.join()
-
-            # wait for next iteration
-            sleep(time_to_seconds(PING_INTERVAL))
 
     @staticmethod
     def ping() -> None:
         """
         sends out a ping (to itself, technically)
         """
-        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client.settimeout(10)
-
-        try:
-            # try to connect to server
-            client.connect((PUBLIC_HOST, PORT))
-            client.recv(8)
+        # try to connect to server
+        if check_connection(PRIVATE_HOST):
             logging.info(f"{strftime('%c')}: ping OK")
 
-        except (TimeoutError, ConnectionRefusedError) as e:
+        else:
             # in case of TimeoutError, restart the ssh_tunnel service
-            logging.warning(f"{strftime('%c')}: ping FAIL: \"{e}\"")
+            logging.warning(f"{strftime('%c')}: ping FAIL")
 
             # try if internet is reachable
-            if check_connection():
+            if check_connection(PUBLIC_HOST):
                 logging.warning(
                     f"{strftime('%c')}: internet reachable, RESTARTING"
                 )
@@ -130,26 +76,23 @@ class PingPong:
             else:
                 logging.error(f"{strftime('%c')}: internet UNREACHABLE")
 
-        except socket.gaierror:
-            logging.error(f"{strftime('%c')}: failure in NAMERESOLUTION")
-
-        finally:
-            client.close()
-
     def run(self) -> None:
         """
         starts both client and server
         """
-        self.accept_ping()
+        logging.info(f"{strftime('%c')}: starting server")
+
+        while self.running:
+            self.ping()
+
+            # wait for next iteration
+            sleep(time_to_seconds(PING_INTERVAL))
 
     def close(self) -> None:
         """
         stop all running processes
         """
         logging.info(f"{strftime('%c')}: shutting down")
-
-        self._server_socket.shutdown(socket.SHUT_RDWR)
-        self._server_socket.close()
         self.running = False
 
         exit(0)
